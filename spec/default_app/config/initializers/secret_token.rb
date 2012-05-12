51ebb07762fba58718b78318eee8371ce0208972
Jeremy Walker <jeremy@meducation.net>
2012-05-12 17:10:34 +0100

    Added dummy_app to test against.

---
+++ spec/default_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DefaultApp::Application.config.secret_token = '8496cda3f8fce5cb1453e1a18ad68834d978950336ac382b2bf950769669f205fc51699eb0729f922f2fee2586ed1896a64c24a07fc2eea4b1a5451b33abd21a'
